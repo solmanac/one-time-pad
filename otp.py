f7651e34898aaf2c8180e46d950bc9aa0eba3eda
solmanac <53344949+solmanac@users.noreply.github.com>
2023-09-02 22:06:02 -0700

    Add files via upload

---
+++ otp.py
@@ -0,0 +1,79 @@
+
+import glob
+import random
+import time
+from bitstring import BitArray, Bits, BitStream
+import os
+import sys
+
+
+def keyboardinputcoinfliprng():
+
+    timestr = time.strftime("%Y%m%d%H%M%S")
+
+    def split4(txt):
+        return " ".join("".join(txt[i : i + 4]) for i in range(0, len(txt), 4))
+
+    userinput = input("type some words and press return: ")
+
+    encodeduserinput = userinput.encode("utf-8")
+
+    msgbits = BitArray(encodeduserinput)
+
+    print("message bits: " + split4(msgbits.bin))
+
+    bitlength = msgbits.len
+    cointosses = input("enter at least " + str(bitlength) + " ones and zeroes: ")
+    cointossbitarray = BitArray(bin=cointosses)
+    cointossbitstream = BitStream(cointossbitarray)
+    key = cointossbitstream.read(bitlength)
+
+    cipher = BitArray(msgbits ^ key)
+
+    cipherfilename = timestr + "msgpart1.txt"
+    keyfilename = timestr + "msgpart2.txt"
+
+    ciphertext = open(cipherfilename, "w")
+    ciphertext.write(cipher.bin)
+    print("message bits: " + split4(msgbits.bin))
+    print("key bits:     " + split4(key.bin))
+    print("cipher bits:  " + split4(cipher.bin))
+    ciphertext.close()
+
+    keytext = open(keyfilename, "w")
+    keytext.write(key.bin)
+    keytext.close()
+
+def decrypt():
+    for filename in glob.glob("*msgpart1.txt"):
+        ciphercontent = open(filename, "r").read()
+    ciphertext = BitArray(bin=ciphercontent)
+
+    for filename in glob.glob("*msgpart2.txt"):
+        keycontent = open(filename, "r").read()
+    keytext = BitArray(bin=keycontent)
+
+    decode = (ciphertext ^ keytext).bytes.decode()
+    print(decode)
+
+
+print(20 * "-")
+print(20 * "-")
+print("1. Encrypt keyboard input to text files, RNG: coin flips entered on keyboard")
+print("2. Decrypt. Make sure the message parts of only the current message")
+print("are in the folder containing the encdec program.")
+print(20 * "-")
+
+choice = input("Enter your choice [1-9] : ")
+
+choice = int(choice)
+
+if choice == 1:
+    print("Encrypting keyboard input...")
+    keyboardinputcoinfliprng()
+elif choice == 2:
+    print("Decrypting...")
+    decrypt()
+
+else:  ## default ##
+    print("Invalid number. Try again...")
